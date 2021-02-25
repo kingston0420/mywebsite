b97187765340be48ceac4e79e7d4f1d90e99c7f2
Kingston <kingston880420@gmail.com>
2021-02-24 16:54:37 -0800

    create 99

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/99">99乘法</router-link>
   </div>
   <router-view/>
 </template>
