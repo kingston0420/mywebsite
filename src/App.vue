91e21c5b20881626d522ffbd5823e8c57e773cd4
Bestian Tang <bestian@gmail.com>
2021-03-11 09:26:49 +0800

    use menu

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="back" :class = "{dark: dark}">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/Project">Project</router-link> |
-      <router-link to="/Resume">Resume</router-link> | 
-      <input type="checkbox" v-model="dark"/> dark mode
+    <div class="ui menu">
+      <router-link class="item" to="/">Home</router-link>
+      <router-link class="item" to="/about">About</router-link>
+      <router-link class="item" to="/Project">Project</router-link>
+      <router-link class="item" to="/Resume">Resume</router-link>
+      <div class="item">
+        <input type="checkbox" v-model="dark"/> dark mode
+      </div>
     </div>
     <div>
       <router-view/>
