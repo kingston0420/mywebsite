db8ad247a5f5e83825fcf592cfe2fe526627669b
Kingston <kingston880420@gmail.com>
2021-04-26 22:00:25 -0700

    Update Resume.vue

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -9,10 +9,11 @@
           <li>Java</li>
           <li>Python</li>
           <li>HTML, CSS, JavaScript</li>
+          <li>Vue.js/framework</li>
+          <li>Android Studio</li>
           <li>Arm and 8051 Assembly</li>
-          <li>R</li>
+          <li>SQL, R</li>
           <li>MATLAB, Simulink</li>
-          <li>SimulationX</li>
           <li>Verilog(ModelSim/Quartus)</li>
         </ul>
       </div>
@@ -35,26 +36,92 @@
         <h2><i class = "paint brush icon"/>Others</h2>
         <ul>
           <li>Bilingual English and Mandarin</li>
-          <li>Git, Git bash</li>
-          <li>Adobe Photoshop/ Illustrator</li>
-          <li>Microsoft Office Word/Excel/PowerPoint</li>
-          <li>3D printing</li>
-          <li>Solidworks</li>
           <li>Knowledge of data structures and algorithms</li>
           <li>Knowledge of Network layering and protocol</li>
+          <li>Git, Git bash</li>
+          <li>Linux</li>
+          <li>Cisco IOS, JunOS</li>
+          <li>Microsoft Office Word/Excel/PowerPoint</li>
+          <li>Adobe Photoshop/ Illustrator</li>
+          <li>Solidworks</li>
+          <li>SimulationX</li>
         </ul>
       </div>
     </div>
 
-    <div class="technicalExperience">
-      <h1>Bell Canada Enterprises </h1>
+    <h1>Technical Work Experience</h1>
+    <div class="technicalWorkExperience">
+      <h2>Bell Canada Enterprises - Vancouver, Canada <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; September, 2020 – September, 2021</span></h2> 
+      <h2>Network Specialist, Solutions Architecture / Software Developer</h2>
+      <ul>
+          <li>Develop and apply technical knowledge on SONET, Ethernet, IP/MPLS network, application layer protocols, data & network virtualization (SDN/NFV) and data analytics technologies</li>
+          <li>Participate in the development and lab verification of new SONET, Ethernet, IP/MPLS and virtualization services and network infrastructure architecture.</li>
+          <li>Construct network circuit based on customer’s need and conduct Layer2 and Layer3 test to make sure each characteristic measurement meet the requirement. Work with Cisco IOS and JunOS CLI.</li>
+          <li>Develop web application for network device management. Create script for firmware update and device configuration.</li>
+      </ul>
     </div>
 
+    <div class="technicalWorkExperience">
+      <h2>Maxim Integrated - Vancouver, Canada <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; July, 2020 – September, 2020</span></h2>
+      <h2>Application Engineer</h2>
+      <ul>
+        <li>Assist in the development of new products from conception, through development, testing and final production</li>
+        <li>Perform system level and hardware level verification, debug, and testing on new products and create automation test system.</li>
+        <li>Write bug reports which clearly detail the problem and how to reproduce it.</li>
+        <li>Research and develop a firm knowledge of USB, DP, RJ45, and RS 232 standards and work with all different groups including hardware design, software design, marketing, and production.</li>
+        <li>Work with Cisco, Netgear, Dell network switches through CLI – configuration, VLAN, subnet, and bandwidth.</li>
+      </ul>
+    </div>
+    
+    <h1>Other Work Experience</h1>
+    <div class="technicalWorkExperience">
+      <h2>Kingston Live With Style - Vancouver, BC <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; May, 2018 – Present</span></h2>
+      <h2>Brand Founder</h2>
+      <ul>
+          <li>Design creative clothing using photoshop and illustrator, as well as manage production line</li>
+          <li>Manage website and Instagram by adding relevant photos, videos, and applying creative marketing skills</li>
+          <li>Communicate effectively with customers and partners to ensure they are satisfied with the products</li>
+      </ul>
+    </div>
+    <div class="technicalWorkExperience">
+      <h2>UBC Unlimited Dance Club - Vancouver, BC <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; April, 2019 – April, 2020</span></h2>
+      <h2>April, 2019 – April, 2020</h2>
+      <ul>
+        <li>Create choreography and teach students the fundamentals and technique of Locking, a style of dance</li>
+      </ul>
+    </div>
+    <div class="technicalWorkExperience">
+      <h2>UBC Recreation - Vancouver, BC <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; December, 2017 – April, 2020</span></h2>
+      <h2>Volleyball and Basketball Referee </h2>
+      <ul>
+          <li>Responsible for signing in players, reporting scores, and presiding over the game from a neutral point of view to enforce the rules</li>
+      </ul>
+    </div>
 
+    <h1>Volunteer Experience</h1>
+    <div class="technicalWorkExperience">
+      <h2>UBC Dance Horizons - Vancouver, BC <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; April, 2018 – Present</span></h2>
+      <h2>Vice President </h2>
+      <ul>
+          <li>Design creative clothing using photoshop and illustrator, as well as manage production line</li>
+          <li>Manage website and Instagram by adding relevant photos, videos, and applying creative marketing skills</li>
+          <li>Communicate effectively with customers and partners to ensure they are satisfied with the products</li>
+      </ul>
+    </div>
+    <div class="technicalWorkExperience">
+      <h2>UBC Taiwanese Association - Vancouver, BC <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; April, 2018 – April, 2020</span></h2>
+      <h2>Marketing Director </h2>
+      <ul>
+        <li>Responsible for creating graphic design of Facebook banners, Instagram posts, Snapchat stories, event posters, handouts, and promo videos</li>
+        <li>Apply advertising skills and constantly on the lookout for the club’s image and reputation</li>
+      </ul>
+    </div>
+    
   </div>
 
   
 </template>
+
 
 <style scoped>
 
@@ -76,7 +143,7 @@
 
 .c {
   display: inline-block;
-  width: 33%;
+  width: 33.333%;
   vertical-align: top;
   padding-bottom: 2em;
   height: 18em;
@@ -92,7 +159,7 @@
   }
 }
 
-ul {
+.row ul {
   margin: 0 auto;
   text-align: left;
   width: 25vw;
@@ -104,5 +171,21 @@ ul {
   }
 }
 
+span {
+  font-size: 0.7em;
+  font-style: italic;
+  font-family: 'Times New Roman', Times, serif;
+  text-align: right;
+}
+
+.technicalWorkExperience {
+  text-align: left;
+  padding: 10px 40px;
+  background-color: cornflowerblue;
+}
+
+.technicalWorkExperience h2 {
+  margin: 5px 0px;
+}
 </style>
 
