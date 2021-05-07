b192fdad22ca4f2a66c2bab26c7ba8ac7116a18c
Kingston <kingston880420@gmail.com>
2021-05-06 18:58:03 -0700

    deploy

---
+++ precache-manifest.b4beaa60a9e2c4e38b2b9cc869b1c48c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81887bc475e25613dd0a2338cf275ca3",
+    "url": "/mywebsite/author.jpg"
+  },
+  {
+    "revision": "005e325c6d39af0fbece",
+    "url": "/mywebsite/css/about.3d628d79.css"
+  },
+  {
+    "revision": "005e325c6d39af0fbece",
+    "url": "/mywebsite/css/about.3d628d79.css.map"
+  },
+  {
+    "revision": "d00e31fc93f78d9a2c66",
+    "url": "/mywebsite/css/app.5501b9b9.css"
+  },
+  {
+    "revision": "d00e31fc93f78d9a2c66",
+    "url": "/mywebsite/css/app.5501b9b9.css.map"
+  },
+  {
+    "revision": "c07a8881d6764392c7080af3c07883d3",
+    "url": "/mywebsite/favicon.ico"
+  },
+  {
+    "revision": "81887bc475e25613dd0a2338cf275ca3",
+    "url": "/mywebsite/img/author.81887bc4.jpg"
+  },
+  {
+    "revision": "7fc5eb3650145bb01aba4c1f7ae08faa",
+    "url": "/mywebsite/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "6370bd2e9e51b45d1e296413a2693201",
+    "url": "/mywebsite/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "7fc5eb3650145bb01aba4c1f7ae08faa",
+    "url": "/mywebsite/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "6370bd2e9e51b45d1e296413a2693201",
+    "url": "/mywebsite/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "0d1592654bf10aa2afbfe3c989b69168",
+    "url": "/mywebsite/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "e4035179802f57f0ed14ffb5763ab4a7",
+    "url": "/mywebsite/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "d3e34f6f10c7475ed3f620f820313aa0",
+    "url": "/mywebsite/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "1badf33abb6ceaa58d0936f74b7ef9ed",
+    "url": "/mywebsite/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "7048d535a298e0e516a29d999dc5a9ca",
+    "url": "/mywebsite/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "d3e34f6f10c7475ed3f620f820313aa0",
+    "url": "/mywebsite/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "451111bffff8d7d37c9889a4d231daa6",
+    "url": "/mywebsite/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "e163d6a1dd2d1530b6499a16cf943d5d",
+    "url": "/mywebsite/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c07a8881d6764392c7080af3c07883d3",
+    "url": "/mywebsite/img/icons/favicon.ico"
+  },
+  {
+    "revision": "53af4cc6c24002e68c31c002065198c7",
+    "url": "/mywebsite/img/icons/manifest.json"
+  },
+  {
+    "revision": "aa34c11daa09353975832547367ea209",
+    "url": "/mywebsite/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "d4ee32138e05a068ca9dba496bd48d46",
+    "url": "/mywebsite/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "a9344ffa7a4c685a4fc98da8e6c3628c",
+    "url": "/mywebsite/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b98ad8802ce2d73df97648a68a79342f",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "005e325c6d39af0fbece",
+    "url": "/mywebsite/js/about.ca368216.js"
+  },
+  {
+    "revision": "005e325c6d39af0fbece",
+    "url": "/mywebsite/js/about.ca368216.js.map"
+  },
+  {
+    "revision": "d00e31fc93f78d9a2c66",
+    "url": "/mywebsite/js/app.776757b0.js"
+  },
+  {
+    "revision": "d00e31fc93f78d9a2c66",
+    "url": "/mywebsite/js/app.776757b0.js.map"
+  },
+  {
+    "revision": "c1a379a823903f8b5524",
+    "url": "/mywebsite/js/chunk-vendors.15da67b4.js"
+  },
+  {
+    "revision": "c1a379a823903f8b5524",
+    "url": "/mywebsite/js/chunk-vendors.15da67b4.js.map"
+  },
+  {
+    "revision": "db81f418ad03235b142192891cf96824",
+    "url": "/mywebsite/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mywebsite/robots.txt"
+  }
+]);
